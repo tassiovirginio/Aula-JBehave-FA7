7eb0ddc5bc8d1a7553ddae5d585d4419140af676
Tassio Virginio <tassiovirginio@gmail.com>
2011-10-22 09:10:25 -0300

    Melhorias no codigo.

--- src/fa7/storys/AlunoComecaAUsarJbehave.java
+++ src/fa7/storys/AlunoComecaAUsarJbehave.java
@@ -1,31 +1,12 @@
 package fa7.storys;
 
-import java.util.List;
-
-import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.io.LoadFromClasspath;
-import org.jbehave.core.junit.JUnitStory;
-import org.jbehave.core.reporters.Format;
-import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.CandidateSteps;
-import org.jbehave.core.steps.InstanceStepsFactory;
-
 import fa7.storys.steps.AlunoComecaAUsarJBehaveSteps;
 
-public class AlunoComecaAUsarJbehave extends JUnitStory {
-	@Override
-	public Configuration configuration() {
-		return new MostUsefulConfiguration()
-				.useStoryLoader(new LoadFromClasspath(this.getClass()))
-				.useStoryReporterBuilder(
-						new StoryReporterBuilder().withDefaultFormats()
-								.withFormats(Format.CONSOLE, Format.TXT));
+public class AlunoComecaAUsarJbehave extends JUnitStoryFA7 {
+	
+	public AlunoComecaAUsarJbehave() {
+		object = this;
+		objectSteps = new AlunoComecaAUsarJBehaveSteps();
 	}
-
-	@Override
-	public List<CandidateSteps> candidateSteps() {
-		return new InstanceStepsFactory(configuration(),
-				new AlunoComecaAUsarJBehaveSteps()).createCandidateSteps();
-	}
+	
 }
